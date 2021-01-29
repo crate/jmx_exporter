e9bdc94cb5cd494682c8ac4380d1c4d55dfd8b95
Michael Kleen <michael.kleen@crate.io>
2021-01-29 16:42:53 +0100

    Add bwc integration tests

--- src/test/java/io/crate/jmx/integrationtests/MetricsITest.java
+++ src/test/java/io/crate/jmx/integrationtests/MetricsITest.java
@@ -22,28 +22,16 @@
 
 package io.crate.jmx.integrationtests;
 
-import org.junit.BeforeClass;
 import org.junit.Test;
 
-import java.net.HttpURLConnection;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.CoreMatchers.not;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MetricsITest extends AbstractITest {
-
-    private static String METRICS_RESPONSE;
-
-    @BeforeClass
-    public static void fetchMetrics() throws Exception {
-        HttpURLConnection connection = (HttpURLConnection) randomJmxUrlFromServers("/metrics").openConnection();
-        assertThat(connection.getResponseCode(), is(200));
-        METRICS_RESPONSE = parseResponse(connection.getInputStream());
-    }
 
     @Test
     public void testQueryStatsMetrics() {
@@ -111,110 +99,95 @@ public class MetricsITest extends AbstractITest {
         }
     }
 
-    private void assertMetricValue(String metricString) {
-        int startIdx = METRICS_RESPONSE.indexOf(metricString);
-        assertThat(metricString + " not found in response", startIdx, greaterThanOrEqualTo(0));
-        int endIdx = METRICS_RESPONSE.indexOf("\n", startIdx);
-        String metricValueStr = METRICS_RESPONSE.substring(startIdx + metricString.length(), endIdx);
-        assertThat(
-                String.format("Metric '%s' value '%s' must be in the format ^-\\d+.\\d+(E\\d+)?", metricString, metricValueStr),
-                metricValueStr.matches("^-?\\d+.\\d+(E\\d+)?"),
-                is(true)
-        );
-    }
-
     @Test
     public void testNodeStatusMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("crate_ready 1.0\n"));
+        assertThat(metricsResponse, containsString("crate_ready 1.0\n"));
     }
 
     @Test
     public void testJvmVersionMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("jvm_info{version="));
+        assertThat(metricsResponse, containsString("jvm_info{version="));
     }
 
     @Test
     public void testJvmMemoryMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_used{area=\"heap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_used{area=\"nonheap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_committed{area=\"heap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_committed{area=\"nonheap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_max{area=\"heap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_max{area=\"nonheap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_init{area=\"heap\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_bytes_init{area=\"nonheap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_used{area=\"heap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_used{area=\"nonheap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_committed{area=\"heap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_committed{area=\"nonheap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_max{area=\"heap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_max{area=\"nonheap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_init{area=\"heap\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_bytes_init{area=\"nonheap\",}"));
 
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"CodeHeap 'non-nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"CodeHeap 'profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"Metaspace\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"Compressed Class Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"G1 Eden Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"G1 Survivor Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_used{pool=\"G1 Old Gen\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"CodeHeap 'non-nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"CodeHeap 'profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"Metaspace\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"Compressed Class Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"G1 Eden Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"G1 Survivor Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_committed{pool=\"G1 Old Gen\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"CodeHeap 'non-nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"CodeHeap 'profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"Metaspace\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"Compressed Class Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"G1 Eden Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"G1 Survivor Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_max{pool=\"G1 Old Gen\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"CodeHeap 'non-nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"CodeHeap 'profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"Metaspace\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"Compressed Class Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"G1 Eden Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"G1 Survivor Space\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_memory_pool_bytes_init{pool=\"G1 Old Gen\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"CodeHeap 'non-nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"CodeHeap 'profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"Metaspace\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"Compressed Class Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"G1 Eden Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"G1 Survivor Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_used{pool=\"G1 Old Gen\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"CodeHeap 'non-nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"CodeHeap 'profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"Metaspace\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"Compressed Class Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"G1 Eden Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"G1 Survivor Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_committed{pool=\"G1 Old Gen\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"CodeHeap 'non-nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"CodeHeap 'profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"Metaspace\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"Compressed Class Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"G1 Eden Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"G1 Survivor Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_max{pool=\"G1 Old Gen\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"CodeHeap 'non-nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"CodeHeap 'profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"CodeHeap 'non-profiled nmethods'\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"Metaspace\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"Compressed Class Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"G1 Eden Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"G1 Survivor Space\",}"));
+        assertThat(metricsResponse, containsString("jvm_memory_pool_bytes_init{pool=\"G1 Old Gen\",}"));
     }
 
     @Test
     public void testJvmThreadsMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("jvm_threads_current"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_threads_daemon"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_threads_peak"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_threads_started_total"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_threads_deadlocked"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_threads_deadlocked_monitor"));
+        assertThat(metricsResponse, containsString("jvm_threads_current"));
+        assertThat(metricsResponse, containsString("jvm_threads_daemon"));
+        assertThat(metricsResponse, containsString("jvm_threads_peak"));
+        assertThat(metricsResponse, containsString("jvm_threads_started_total"));
+        assertThat(metricsResponse, containsString("jvm_threads_deadlocked"));
+        assertThat(metricsResponse, containsString("jvm_threads_deadlocked_monitor"));
     }
 
     @Test
     public void testJvmGcMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("jvm_gc_collection_seconds_count{gc=\"G1 Young Generation\",}"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_gc_collection_seconds_sum{gc=\"G1 Young Generation\",}"));
+        assertThat(metricsResponse, containsString("jvm_gc_collection_seconds_count{gc=\"G1 Young Generation\",}"));
+        assertThat(metricsResponse, containsString("jvm_gc_collection_seconds_sum{gc=\"G1 Young Generation\",}"));
     }
 
     @Test
     public void testJvmClassLoadingMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("jvm_classes_loaded"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_classes_loaded_total"));
-        assertThat(METRICS_RESPONSE, containsString("jvm_classes_unloaded_total"));
+        assertThat(metricsResponse, containsString("jvm_classes_loaded"));
+        assertThat(metricsResponse, containsString("jvm_classes_loaded_total"));
+        assertThat(metricsResponse, containsString("jvm_classes_unloaded_total"));
     }
 
     @Test
     public void testJvmProcessMetrics() {
-        assertThat(METRICS_RESPONSE, containsString("process_cpu_seconds_total"));
-        assertThat(METRICS_RESPONSE, containsString("process_start_time_seconds"));
-        assertThat(METRICS_RESPONSE, containsString("process_open_fds"));
-        assertThat(METRICS_RESPONSE, containsString("process_max_fds"));
+        assertThat(metricsResponse, containsString("process_cpu_seconds_total"));
+        assertThat(metricsResponse, containsString("process_start_time_seconds"));
+        assertThat(metricsResponse, containsString("process_open_fds"));
+        assertThat(metricsResponse, containsString("process_max_fds"));
     }
 
     @Test
     public void testRequestingMultipleTimesDoesNotResultInDuplicateMetrics() throws Exception {
-        HttpURLConnection connection = (HttpURLConnection) randomJmxUrlFromServers("/metrics").openConnection();
-        assertThat(connection.getResponseCode(), is(200));
-        String response = parseResponse(connection.getInputStream());
-
+        String response = parseMartricsResponse();
         assertThat(response, containsString("crate_ready 1.0\n"));
         assertThat(response, not(containsString("crate_ready 1.0\ncrate_ready 1.0\n")));
     }
